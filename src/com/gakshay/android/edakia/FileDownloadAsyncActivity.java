ca5c5a565380f005de71e6908224ac00165717dc
Amit sharma <emailamitsharma@yahoo.com>
2012-11-04 16:24:57 +0530

    Dialogs Types + FileDownload

---
+++ src/com/gakshay/android/edakia/FileDownloadAsyncActivity.java
@@ -0,0 +1,113 @@
+package com.gakshay.android.edakia;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+
+public class FileDownloadAsyncActivity extends Activity {
+
+	private String downloadURL;
+	private String absolutefilePath;
+	public static final int DIALOG_DOWNLOAD_PROGRESS = 0;
+	private ProgressDialog mProgressDialog;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_file_download_async);
+		downloadURL = ((String)getIntent().getExtras().get("downloadURL"));
+		absolutefilePath = ((String)getIntent().getExtras().get("filePath"));
+		startDownload();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_file_download_async, menu);
+		return true;
+	}
+
+	private void startDownload() {
+		new DownloadFileAsync().execute(downloadURL);
+	}
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case DIALOG_DOWNLOAD_PROGRESS:
+			mProgressDialog = new ProgressDialog(this);
+			mProgressDialog.setMessage("Downloading file..");
+			mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+			mProgressDialog.setCancelable(false);
+			mProgressDialog.show();
+			return mProgressDialog;
+		default:
+			return null;
+		}
+	}
+
+	class DownloadFileAsync extends AsyncTask<String, String, String> {
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			showDialog(DIALOG_DOWNLOAD_PROGRESS);
+		}
+
+		@Override
+		protected String doInBackground(String... aurl) {
+			int count;
+
+			try {
+
+				URL url = new URL(aurl[0]);
+				URLConnection conexion = url.openConnection();
+				conexion.connect();
+
+				int lenghtOfFile = conexion.getContentLength();
+				Log.d("ANDRO_ASYNC", "Lenght of file: " + lenghtOfFile);
+
+				InputStream input = new BufferedInputStream(url.openStream());
+				OutputStream output = new FileOutputStream(absolutefilePath);
+
+				byte data[] = new byte[1024];
+
+				long total = 0;
+
+				while ((count = input.read(data)) != -1) {
+					total += count;
+					publishProgress(""+(int)((total*100)/lenghtOfFile));
+					output.write(data, 0, count);
+				}
+
+				output.flush();
+				output.close();
+				input.close();
+			} catch (Exception e) {
+
+				Log.d("error", e.toString());
+			}
+			return null;
+
+		}
+		protected void onProgressUpdate(String... progress) {
+			Log.d("ANDRO_ASYNC",progress[0]);
+			mProgressDialog.setProgress(Integer.parseInt(progress[0]));
+		}
+
+		@Override
+		protected void onPostExecute(String unused) {
+			dismissDialog(DIALOG_DOWNLOAD_PROGRESS);
+		}
+	}
+}
+
---
+++ src/com/gakshay/android/util/CustomDialog.java
@@ -0,0 +1,173 @@
+/**
+ * 
+ */
+package com.gakshay.android.util;
+
+
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * @author Amitsharma
+ *
+ */
+public class CustomDialog extends Dialog{
+
+	public CustomDialog(Context context, int theme) {
+		super(context, theme);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static Dialog resultCostDialog(Context dialogContext,int dialogTheme, int dialogTitle, int dialogLayout, int dialogButton,int dialogMsg,String dialogResultMsg){
+		final Dialog dialog = new Dialog(dialogContext, dialogTheme);
+		dialog.requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+		dialog.setContentView(dialogLayout);
+		dialog.getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, dialogTitle);
+
+		((TextView) dialog.findViewById(dialogMsg)).setText(dialogResultMsg);
+		
+		dialog.setCancelable(false);
+
+		//set up button
+		Button button = (Button) dialog.findViewById(dialogButton);
+
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog.dismiss();   
+				//finish();
+			}
+		});
+		//now that the dialog is set up, it's time to show it    
+		return dialog;	
+	}
+
+
+	public static Dialog resultCostDialog(Context dialogContext,int dialogTheme, int dialogTitle, int dialogLayout, int dialogButton,int dialogMsg,String dialogResultMsg,
+			int trnsCost,int trnsAmount,int trnsBalance, String trnsAmountValue, String userBalance, String trnsCostValue){
+		final Dialog dialog = new Dialog(dialogContext, dialogTheme);
+		dialog.requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+		dialog.setContentView(dialogLayout);
+		dialog.getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, dialogTitle);
+
+		dialog.setCancelable(false);
+
+		((TextView) dialog.findViewById(dialogMsg)).setText(dialogResultMsg);
+
+		((TextView) dialog.findViewById(trnsCost)).setText(trnsCostValue);
+
+		((TextView) dialog.findViewById(trnsAmount)).setText(trnsAmountValue);
+		((TextView) dialog.findViewById(trnsBalance)).setText(userBalance);
+
+		//set up button
+		Button button = (Button) dialog.findViewById(dialogButton);
+
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog.dismiss();   
+			}
+		});
+		return dialog;	
+	}
+
+
+
+	public static Dialog resultChngPwdDialog(Context dialogContext,int dialogTheme, int dialogTitle, int dialogLayout, int dialogButton,int dialogMsg,String dialogResultMsg){
+		final Dialog dialog = new Dialog(dialogContext, dialogTheme);
+		dialog.requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+		dialog.setContentView(dialogLayout);
+		dialog.getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, dialogTitle);
+		dialog.setCancelable(false);
+
+		((TextView) dialog.findViewById(dialogMsg)).setText(dialogResultMsg);
+
+
+		//set up button
+		Button button = (Button) dialog.findViewById(dialogButton);
+
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog.dismiss();   
+			}
+		});
+		return dialog;	
+	}
+	
+	
+	public static Dialog resultGenericErrorDialog(Context dialogContext,int dialogTheme, int dialogTitle, int dialogLayout, int dialogButton,int dialogMsg,String dialogResultMsg){
+		final Dialog dialog = new Dialog(dialogContext, dialogTheme);
+		dialog.requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+		dialog.setContentView(dialogLayout);
+		dialog.getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, dialogTitle);
+
+		dialog.setCancelable(true);
+
+		((TextView) dialog.findViewById(dialogMsg)).setText(dialogResultMsg);
+
+
+		//set up button
+		Button button = (Button) dialog.findViewById(dialogButton);
+
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog.dismiss();   
+			}
+		});
+		return dialog;	
+	}
+
+
+	/*private void prepareAlertDialog(int dialogTheme,int dialogTitle,int dialogIcon,int dialogButton){
+		AlertDialog.Builder altDialog= new AlertDialog.Builder(dialogContext,dialogTheme);
+		altDialog.setTitle(gets);
+
+		altDialog.setInverseBackgroundForced(true);
+
+		LayoutInflater inflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		v = inflater.inflate(R.layout.player_info, null, false);
+		ViewGroup.LayoutParams p = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+		dialog.setContentView(v,p);
+
+		LayoutInflater inflater = getLayoutInflater();
+		View dialoglayout = inflater.inflate(R.layout.basic_dialog_layout, (ViewGroup) getCurrentFocus());
+		altDialog.setView(dialoglayout);	
+
+		altDialog.setMessage("Alert Dialog Message"); // here add your message
+		altDialog.setCancelable(false);
+		altDialog.setIcon(R.drawable.ic_launcher);
+		altDialog.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				//do nothing.
+				dialog.cancel();
+				dialog.dismiss();
+			}
+		});
+
+		altDialog.show();
+		AlertDialog resultDialog = altDialog.create();
+		resultDialog.requestWindowFeature(Window.PROGRESS_END);
+
+		resultDialog.onWindowFocusChanged(false);
+		resultDialog.setCancelable(false);
+		Window window = resultDialog.getWindow();
+		WindowManager.LayoutParams wlp = window.getAttributes();
+		wlp.gravity = Gravity.CENTER;
+		//wlp.flags &= ~WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+		window.setAttributes(wlp);
+
+		resultDialog.show();
+	}
+	 */
+
+}
